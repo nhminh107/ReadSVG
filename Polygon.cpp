940d00b025f855889bad608ab5d06ffe98e9f39d
nhminh107 <24120381@student.hcmus.edu.vn>
2025-11-02 15:59:55 +0700

    Thêm tính năng vẽ polyline

--- Polygon.cpp
+++ Polygon.cpp
@@ -1,47 +1,32 @@
 ﻿#include "Polygon.h"
-
 using namespace std;
 
-
-
 void CPolygon::draw(sf::RenderWindow& window) const {
+    if (pointList.empty()) return;
 
-	auto fColor = fillColor.to_sfml_color();
+    vector<Point> transformed_points = pointList;
+    for (Point& p : transformed_points)
+        p.applyTransform(finalMatrix);
 
-	sf::ConvexShape polygon;
+    // Nếu là polygon (đóng), dùng ConvexShape
+    if (isClosed) {
+        sf::ConvexShape polygon;
+        polygon.setPointCount(transformed_points.size());
+        for (size_t i = 0; i < transformed_points.size(); ++i)
+            polygon.setPoint(i, sf::Vector2f(transformed_points[i].xPoint, transformed_points[i].yPoint));
 
-
-	std::vector<Point> transformed_points = pointList;
-
-
-	for (Point& p : transformed_points) {
-
-		p.applyTransform(finalMatrix);
-
-	}
-
-
-
-	polygon.setPointCount(transformed_points.size());
-
-	for (int i = 0; i < transformed_points.size(); i++) {
-
-		polygon.setPoint(i, sf::Vector2f(transformed_points[i].xPoint, transformed_points[i].yPoint));
-
-	}
-
-
-
-	polygon.setFillColor(fillColor.to_sfml_color());
-
-	polygon.setOutlineColor(strokeColor.to_sfml_color());
-
-	polygon.setOutlineThickness(strokeWidth);
-
-
-
-
-
-	window.draw(polygon);
-
+        polygon.setFillColor(fillColor.to_sfml_color());
+        polygon.setOutlineColor(strokeColor.to_sfml_color());
+        polygon.setOutlineThickness(strokeWidth);
+        window.draw(polygon);
+    }
+    // Nếu là polyline (mở), dùng VertexArray để chỉ vẽ đường viền
+    else {
+        sf::VertexArray polyline(sf::LineStrip, transformed_points.size());
+        for (size_t i = 0; i < transformed_points.size(); ++i) {
+            polyline[i].position = sf::Vector2f(transformed_points[i].xPoint, transformed_points[i].yPoint);
+            polyline[i].color = strokeColor.to_sfml_color();
+        }
+        window.draw(polyline);
+    }
 }
--- Polygon.h
+++ Polygon.h
@@ -6,11 +6,15 @@ using namespace std;
 
 class CPolygon : public CShape {
 private:
-	vector<Point> pointList;
+    vector<Point> pointList;
+    bool isClosed; // Thêm cờ nhận biết polygon hay polyline
+
 public:
+    CPolygon(const vector<Point>& pList, float sBorder, const Color& fColor,
+        const Color& sColor, const Matrix& m, bool closed = true)
+        : CShape(fColor, sColor, sBorder, m),
+        pointList(pList),
+        isClosed(closed) {}
 
-	CPolygon(const vector<Point>& pList, float sBorder, const Color& fColor, const Color& sColor, const Matrix& m) :
-		CShape(fColor, sColor, sBorder, m), pointList(pList) {}
-	void draw(sf::RenderWindow& window) const override;
-
+    void draw(sf::RenderWindow& window) const override;
 };
