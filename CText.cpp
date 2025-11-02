ea5f1955e6ca841c3777fb82f1d376ddeb2bda01
nhminh107 <24120381@student.hcmus.edu.vn>
2025-11-02 12:29:24 +0700

    update draw Ctext

--- CText.cpp
+++ CText.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 void CText::draw(sf::RenderWindow& window) const {
-	
+    sf::Text textSF;
+    textSF.setFont(font); // dùng font member
+    textSF.setString(content);
+    textSF.setCharacterSize(static_cast<unsigned int>(fontSize));
+    textSF.setFillColor(fillColor.to_sfml_color());
+    textSF.setPosition(place.xPoint, place.yPoint);
+
+    window.draw(textSF);
 }
--- CText.h
+++ CText.h
@@ -1,36 +1,23 @@
 ﻿#pragma once
-#include <iostream>
-#include "SVGTypes.h"
 #include "CShape.h"
+#include <SFML/Graphics.hpp>
 #include <string>
-
-using namespace std;
 
 class CText : public CShape {
 private:
-    string content;      
-    Point startPoint;     // Tọa độ bắt đầu (x, y)
-    string fontFamily;
+    std::string content;
+    std::string fontFamily;
     float fontSize;
+    Point place;
+    mutable sf::Font font; // ✅ Giữ font sống lâu
 
 public:
-    CText(
-        const string& textContent,
-        const Point& start,             // Vị trí
-        const string& fontFam = "times.ttf",          // Font Family
-        float size,                     // Font Size
-        const Color& fColor,            // Màu nền (Fill/Text color)
-        const Color& sColor,            // Màu viền (Stroke color)
-        float sWidth,                   // Độ dày nét viền (Stroke width)
-        const Matrix& fMatrix           // Ma trận biến đổi
-    ) :
-        content(textContent),
-        startPoint(start),
-        fontFamily(fontFam),
-        fontSize(size),
-        CShape(fColor, sColor, sWidth, fMatrix)
-        // NOTE: Mặc dù <text> không có stroke/strokeWidth, ta vẫn truyền vào để giữ tính nhất quán của CShape.
-    {}
-
+    CText(std::string text, std::string font, float size, float x, float y,
+        const Color& fColor, const Color& sColor, float sWidth, const Matrix& m)
+        : CShape(fColor, sColor, sWidth, m),
+        content(std::move(text)), fontFamily(std::move(font)), fontSize(size), place(x, y)
+    {
+        this->font.loadFromFile(fontFamily);
+    }
     void draw(sf::RenderWindow& window) const override;
 };
